1f4ea3cde6f51c8c6dc63461d5c0eacfd3a598ac
Erica Bohler <ericac.moore@gmail.com>
2015-10-06 08:39:45 -0700

    Update Student Profile Display

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
    root 'home#index'
-   get 'home' => 'home#index'
+   # get 'home' => 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
